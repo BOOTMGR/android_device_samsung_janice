05ad75ff4814f0a71bc1e72ab19b678adbb44a9b
BOOTMGR <harsh.dude16@gmail.com>
2014-03-14 10:15:34 +0530

    Initial commit Fork from https://github.com/diego-ch/android_device_samsung_janice

---
+++ BoardConfig.mk
@@ -0,0 +1,14 @@
+-include device/samsung/u8500-common/BoardCommonConfig.mk
+
+# Vibrator
+BOARD_HAS_VIBRATOR_IMPLEMENTATION := ../../device/samsung/janice/vibrator/vibrator.c
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/samsung/u8500
+TARGET_KERNEL_CONFIG := cyanogenmod_i9070_defconfig
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := janice,i9070,GT-I9070
+
+# Inherit from the proprietary version
+-include vendor/samsung/janice/BoardConfigVendor.mk
