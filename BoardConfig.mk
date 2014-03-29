e9082eae0c7e9c7698a9ef542c857211a1cba64d
BOOTMGR <harsh.dude16@gmail.com>
2014-03-29 20:38:26 +0530

    Build STE Kernel as part of android build
    Signed-off-by: BOOTMGR <harsh.dude16@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,7 @@ BOARD_HAS_VIBRATOR_IMPLEMENTATION := ../../device/samsung/janice/vibrator/vibrat
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/u8500
 TARGET_KERNEL_CONFIG := cyanogenmod_i9070_defconfig
+TARGET_USE_ST_ERICSSON_KERNEL := true
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := janice,i9070,GT-I9070
